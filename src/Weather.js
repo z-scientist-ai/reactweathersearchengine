36486c5eac1be08376a090a546ceeff475bb1531
z-scientist-ai <82810150+z-scientist-ai@users.noreply.github.com>
2023-04-22 20:29:10 +0430

    delete 3

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
